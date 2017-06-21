b7a4493f093f200a019b62d82050aa2fa4da93c2
Jhonny Vanckruz <jhonnyvanckruz@gmail.com>
2017-06-21 13:11:58 -0400

    Empezando con una interfaz de la app, componente header y se añadio react-bootstrap 4

---
+++ src/components/header/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+          <h1>Jhonny React retiros app</h1>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <polygon className="svg--sm" fill="#f1f1f1" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
+            <polygon className="svg--lg" fill="#f1f1f1" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
+          </svg>
+      </header>
+    );
+  }
+}
+
+export default Header;
